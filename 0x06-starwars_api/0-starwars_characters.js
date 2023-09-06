364bfca42a61e266f38dcc867fd91024e732b18d
Ironlaydee <113660966+Ironlaydee@users.noreply.github.com>
2023-09-06 16:47:04 +0100

    characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,25 @@
-Luke Skywalker
-C-3PO
-R2-D2
-Darth Vader
-Leia Organa
-Obi-Wan Kenobi
-Chewbacca
-Han Solo
-Jabba Desilijic Tiure
-Wedge Antilles
-Yoda
-Palpatine
-Boba Fett
-Lando Calrissian
-Ackbar
-Mon Mothma
-Arvel Crynyd
-Wicket Systri Warrick
-Nien Nunb
-Bib Fortuna
+#!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
